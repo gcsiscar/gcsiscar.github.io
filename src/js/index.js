c8fb9744f75d5623cf2412ecd4ed8a0a7a6d61b4
gcsiscar <gilbertchristophersiscar@gmail.com>
2023-06-13 16:15:33 +0800

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+const navItems = document.querySelectorAll('.nav__item');
+const sections = document.querySelectorAll('section, main');
+const headerHeight = document.querySelector('header').scrollHeight;
+let currentSection = '';
+let prevSection = '';
+const sectionOrder = ['home', 'about', 'projects', 'contact'];
+
+window.addEventListener('scroll', (e) => {
+  sections.forEach((section) => {
+    const sectionPos = Math.ceil(window.scrollY);
+    const sectionTop = section.offsetTop - headerHeight;
+    const sectionHeight = section.scrollHeight;
+    if (sectionPos >= sectionTop && sectionPos < sectionTop + sectionHeight) {
+      currentSection = section.id;
+      if (prevSection !== currentSection) {
+        const idx_prevSection = sectionOrder.indexOf(prevSection);
+        const idx_currentSection = sectionOrder.indexOf(currentSection);
+        console.table({
+          idx_currentSection,
+          idx_prevSection,
+          difference: Math.abs(idx_currentSection - idx_prevSection),
+        });
+        if (Math.abs(idx_prevSection - idx_currentSection) === 1) {
+          navItems.forEach((item) => item.classList.remove('active'));
+          const currentItem = document.querySelector(
+            `[href="#${section.id}"]`
+          ).parentElement;
+          currentItem.classList.add('active');
+        }
+        prevSection = currentSection;
+      }
+    }
+  });
+});
+// navItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     navItems.forEach((item) => item.classList.remove('active'));
+//     e.currentTarget.classList.add('active');
+//   });
+// });
+
+// add scroll-padding to navbar
+const html = document.querySelector('html');
+html.style.setProperty('scroll-padding-top', `${headerHeight}px`);
